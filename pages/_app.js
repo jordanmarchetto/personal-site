b025e7d1be7d3ac05274021b5d84a48d6c2d34a2
Jordan Marchetto <jordan.marchetto@ncr.com>
2021-07-28 14:04:16 -0400

    add webvitals reporting

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,19 @@ import MainLayout from '../components/MainLayout'
 import Head from 'next/head';
 import '../styles/globals.css'
 
+export function reportWebVitals({ id, name, label, value }) {
+  // Use `window.gtag` if you initialized Google Analytics as this example:
+  // https://github.com/vercel/next.js/blob/canary/examples/with-google-analytics/pages/_document.js
+  //console.log(`sending metric to google: {id: ${id}, name: ${name}, label: ${label}, value: ${value}} `)
+  window.gtag('event', name, {
+    event_category:
+      label === 'web-vital' ? 'Web Vitals' : 'Web Vital (custom metric)',
+    value: Math.round(name === 'CLS' ? value * 1000 : value), // values must be integers
+    event_label: id, // id unique to current page load
+    non_interaction: true, // avoids affecting bounce rate.
+  })
+}
+
 function MyApp({ Component, pageProps }) {
   const ga_id = "UA-132107286-1";
   const gtm_id = "GTM-5J67PNS";
