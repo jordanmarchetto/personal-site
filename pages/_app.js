4f74c06d452b96aab705c2d2dade669a62aa6c70
Jordan Marchetto <jordan.marchetto@ncr.com>
2021-07-28 10:06:36 -0400

    add google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,28 @@
 import MainLayout from '../components/MainLayout'
+import Head from 'next/head';
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
+  const tracking_id = "UA-132107286-1";
+
   return (
-    <MainLayout {...pageProps}>
-      <Component {...pageProps} />
-    </MainLayout>)
+    <>
+      <Head>
+        <script
+          async
+          src={"https://www.googletagmanager.com/gtag/js?id=" + tracking_id}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', '${tracking_id}', { page_path: window.location.pathname });`,
+          }}
+        />
+      </Head>
+      <MainLayout {...pageProps}>
+        <Component {...pageProps} />
+      </MainLayout>
+    </>
+  )
 }
 
 export default MyApp
