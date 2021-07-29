5566874842541b2c7098715bff1fb503035bd4ae
Jordan Marchetto <jordan.marchetto@ncr.com>
2021-07-29 16:29:47 -0400

    updated resume

--- pages/resume.js
+++ pages/resume.js
@@ -6,14 +6,14 @@ const Resume = () => {
       <Heading pageTitle="Resume" />
       <h2>Resume</h2>
       <p>My resume is available below to view or <a href="/resume.pdf">download</a>.</p>
-      <object data='/resume.pdf#zoom=60&amp;scrollbar=0&amp;toolbar=1&amp;messages=0'
+      <object data='/jordan_marchetto_resume.pdf#zoom=60&amp;scrollbar=0&amp;toolbar=1&amp;messages=0'
         type='application/pdf'
         width='100%'
         height='600' >
-        <param name="src" value="files/Resume.pdf" />
+        <param name="src" value="/jordan_marchetto_resume.pdf" />
         <param name="type" value="application/pdf" />
         <p>It appears your Web browser is not configured to display PDF files.
-          No worries, just <a href='/resume.pdf'>click here to download the PDF file.</a></p>
+          No worries, just <a href='/jordan_marchetto_resume.pdf'>click here to download the PDF file.</a></p>
       </object>
     </div>
   );
