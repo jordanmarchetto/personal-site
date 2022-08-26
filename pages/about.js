f7461f737bfd4ccb37443534e539a54425474913
Jordan Marchetto <jmarchetto@freshtechnology.com>
2022-08-26 09:00:51 -0400

    update for fresh

--- pages/about.js
+++ pages/about.js
@@ -7,7 +7,7 @@ const About = () => {
     <div>
       <Heading pageTitle="About"/>
       <h2>About</h2>
-      <p>I am currently a Senior Web Developer for NCR.
+      <p>I am currently a Senior Software Developer for Fresh Technology.
         If you&apos;re interested in hiring me, or more information on me,
         please view my <Link href="/resume"><a>resume</a></Link> or <Link href="/contact"><a>contact information</a></Link>. 
       </p>
