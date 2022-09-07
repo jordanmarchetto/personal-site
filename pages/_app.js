c8aa4305d23fe81ec5903e200034dcaf56dab923
Jordan Marchetto <jmarchetto@freshtechnology.com>
2022-09-07 09:57:36 -0400

    undo ga4 for now

--- pages/_app.js
+++ pages/_app.js
@@ -16,8 +16,8 @@ export function reportWebVitals({ id, name, label, value }) {
 }
 
 function MyApp({ Component, pageProps }) {
-  //const ga_id = "UA-132107286-1";
-  const ga_id = "G-330862801";
+  const ga_id = "UA-132107286-1";
+  //const ga_id = "G-330862801"; //GA4 tag, I'll implement this later
   const gtm_id = "GTM-5J67PNS";
   return (
     <>
