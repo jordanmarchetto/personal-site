d3b5a4d15732bb949b3854d9b1e1cb3e0821a27c
Jordan Marchetto <jordan.marchetto@gmail.com>
2023-10-27 15:58:32 -0400

    sample change

--- pages/about.js
+++ pages/about.js
@@ -8,7 +8,7 @@ const About = () => {
       <Heading pageTitle="About"/>
       <h2>About</h2>
       <p>I am currently a Senior Software Engineer for Storable.
-        If you&apos;re interested in hiring me, or more information on me,
+        If you are interested in hiring me, or more information on me,
         please view my <Link legacyBehavior href="/resume"><a>resume</a></Link> or <Link legacyBehavior href="/contact"><a>contact information</a></Link>. 
       </p>
     </div>
